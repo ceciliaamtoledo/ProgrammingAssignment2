745d06336a32fb5960f0a500dcfde7a8832b0682
ceciliaamtoledo <cecilia.a.marques@gmail.com>
2015-05-24 18:05:46 -0300

    CacheMatrixReview
    Review from save function

--- cachematrix.R
+++ cachematrix.R
@@ -21,12 +21,19 @@ cacheSolve <- function(x, ...){
         ##Search if the inverse is already in cache and retrieve from the cache
         m <- x$getsolve()       
         if(!is.null(m)) {
-                message("getting cached data")
+                message("getting cached inverse")
                 return(m)
         }
-        ## If not, it will  and returns the inverse from data
+        ## If not, it will run again and returns the inverse from data
         data <- x$get()
         m <- solve(data, ...)
         x$setsolve(m)
         m    
 }
+
+##testing
+##a <- makeCacheMatrix(matrix(rnorm(100), 10, 10))
+##cacheSolve(a)
+##if matrix is changend
+##c <- makeCacheMatrix(matrix(rnorm(50), 5, 5))
+##cacheSolve(c)
